7cbf460b90d759c66046dced1746792de1ca9caa
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-02-18 16:34:00 +0000

    Se creó la clase MétodosArreglos

--- Clase03/MetodosAreglos.java
+++ Clase03/MetodosAreglos.java
@@ -1,23 +1,24 @@
 package Clase03;
 
 public class MetodosAreglos {
-    public String nombresSueldos (String[] n, int[] s){
-        int suma =0;
+    public String nombresSueldos(String[] n, int[] s){
+    int suma = 0;
+        for (int i = 0; i < s.length; i++) {
+            suma += s[i];
+        }
+        double promedio = suma / s.length;
+        String cad = "";
+        int contador = 0;
 
-        for (int i = 0; i<s.length; i++){
-            double promedio = suma/s.length; 
-            String cad ="";
-            int contador = 0;
-            for(int i = 0; i< s.length; i++){
-                if(s[i]>= promedio){
-                    cad += n[i] + "\n";
-                    contador ++;
-
-                }
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] >= promedio) {
+                cad += n[i] + "\n";
+                contador++;
             }
-            cad+= "Fueron " + contador + "los empleados que tiene por encima del promedio: " + promedio;
-            return cad;
+        }
+        cad += "Fueron " + contador + " los empleados que tienen sueldo por encima del promedio: " + promedio;
+        return cad;
         }
 
     }
-}
+
