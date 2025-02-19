b6cb42ad3e524934938f636639cf353cfc580faf
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-02-19 23:35:35 +0000

    Se agregó el método para generar la matriz n x m.

--- Clase03/EjemploMatrices.java
+++ Clase03/EjemploMatrices.java
@@ -1,5 +1,7 @@
 package Clase03;
 
+import java.util.Scanner;
+
 public class EjemploMatrices {
     public static void main (String[] args){
         int[][] m = {{9, 3, 5}, 
@@ -19,7 +21,6 @@ public class EjemploMatrices {
                         }
                         System.out.println("Fila " + i + ": " + sumaFila);
                     }
-                    
                 
                     System.out.println("\nSuma de cada Columna:");
                     for (int j = 0; j < m.length; j++) {
@@ -29,10 +30,60 @@ public class EjemploMatrices {
                         }
                         System.out.println("Columna " + j + ": " + sumaColumnas);
                     }
+
+
+                    int[][] tabla = new int[10][10];
+
+                    for (int i = 0; i < 10; i++) {
+                        for (int j = 0; j < 10; j++) {
+                            tabla[i][j] = (i + 1) * (j + 1);
+                        }
+                    }
+            
+                    System.out.println("Tabla de multiplicar del 1 al 10:");
+                    for (int i = 0; i < 10; i++) {
+                        for (int j = 0; j < 10; j++) {
+                            System.out.printf("%4d", tabla[i][j]);
+                        }
+                    
+                        System.out.println();
+                    }
+                }
+                    {
+                
+                     Scanner scanner = new Scanner(System.in);
+                System.out.print("Ingrese el número de filas (n): ");
+                int n = scanner.nextInt();
+                System.out.print("Ingrese el número de columnas (m): ");
+                int m = scanner.nextInt();
+                
+                int[][] matriz = new int[n][m];
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        if (i == j) { 
+                            matriz[i][j] = 1;
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                    }
+                }
+
+                System.out.println("Matriz generada:");
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        System.out.print(matriz[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                
+                scanner.close();
+    }
         
                  }
+                
 
 
-    }
+    
     
 
