715c6c1eaa4c915415bb1b361fa3df30004190a4
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-04-01 16:29:17 +0000

    Se agregaron lños métodos toString y get

--- Trabajador.java
+++ Trabajador.java
@@ -9,4 +9,23 @@ public class Trabajador {
         this.Salario = Salario;
 
      }
+
+      public String getCedula(){
+        return cedula;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public String toString(){
+        return "Trabajador { Cedula: " + cedula + " Nombre: " + nombre + " Salario: " + salario + "}"; 
+    }
+
+    public double pagar(double incremento){
+        return salario + (salario * incremento);
 }
