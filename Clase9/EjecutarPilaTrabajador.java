b73acef05d1275322f1265638d7c9d7bc3cadec7
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-04-01 16:52:06 +0000

    Se agregó el método para imprimir los elementos de la pila

---
+++ Clase9/EjecutarPilaTrabajador.java
@@ -0,0 +1,21 @@
+import java.util.stack;
+public class EjecutarPilaTrabajador{
+    public static void main (String[] args){
+
+        stack<Trabajador> t= new stack<>();
+
+        Trabajador t1 = new Trabajador ("1234", "Juan", "2200.0"); 
+
+        Trabajador t2 = new Trabajador ("2222", "Luis", "1200.0"); 
+
+        Trabajador t3 = new Trabajador ("0000", "Brr", "3300.0"); 
+
+        Trabajadores.push("t1");
+        Trabajadores.push("t2");
+        Trabajadores.push("t3");
+
+        System.out.println("Trabajadores");
+
+
+    }
+}
