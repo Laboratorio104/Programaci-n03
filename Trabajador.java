d54b0e7f55d5dfca78bf2605e9a0445af58acc2a
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-04-01 16:25:39 +0000

    Se agregaron los atributos de la clase Trabajador

---
+++ Trabajador.java
@@ -0,0 +1,12 @@
+public class Trabajador {
+     private String Cedula; 
+     private String Nombre;
+     private double Salario;
+
+     publi Trabajador(String Cedula, String Nombre, double Salario){
+        this.Cedula = Cedula;
+        this.Nombre = Nombre;
+        this.Salario = Salario;
+
+     }
+}
